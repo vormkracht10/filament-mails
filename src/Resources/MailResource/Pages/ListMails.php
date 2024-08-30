9118515991184c7a6a2b0aacad1767d04265e25d
Baspa <hello@baspa.dev>
2024-08-30 11:43:33 +0200

    Add tabs

--- src/Resources/MailResource/Pages/ListMails.php
+++ src/Resources/MailResource/Pages/ListMails.php
@@ -4,6 +4,9 @@ namespace Vormkracht10\FilamentMails\Resources\MailResource\Pages;
 
 use Filament\Resources\Pages\ListRecords;
 use Vormkracht10\FilamentMails\Resources\MailResource;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use Vormkracht10\FilamentMails\Models\Mail;
 
 class ListMails extends ListRecords
 {
@@ -18,5 +21,39 @@ class ListMails extends ListRecords
     protected function getActions(): array
     {
         return [];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->label(__('All'))
+                ->icon('heroicon-o-inbox')
+                ->badge(Mail::count()),
+            'sent' => Tab::make()
+                ->label(__('Sent'))
+                ->icon('heroicon-o-paper-airplane')
+                ->badge(Mail::sent()->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->sent()),
+            'delivered' => Tab::make()
+                ->label(__('Delivered'))
+                ->icon('heroicon-o-check-circle')
+                ->badge(Mail::delivered()->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->delivered()),
+            'bounced' => Tab::make()
+                ->label(__('Bounced'))
+                ->icon('heroicon-o-x-circle')
+                ->badge(fn() => Mail::softBounced()->count() + Mail::hardBounced()->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->where(function ($query) {
+                    $query->softBounced()->orWhere(function ($query) {
+                        $query->hardBounced();
+                    });
+                })),
+            'unsent' => Tab::make()
+                ->label(__('Unsent'))
+                ->icon('heroicon-o-x-circle')
+                ->badge(Mail::unsent()->count())
+                ->modifyQueryUsing(fn(Builder $query) => $query->unsent()),
+        ];
     }
 }
