1fde133bf8614425af372bafbbc9fda460cfd4bc
Baspa <hello@baspa.dev>
2024-09-03 15:58:43 +0200

    Infolist data

--- src/Resources/EventResource.php
+++ src/Resources/EventResource.php
@@ -2,13 +2,17 @@
 
 namespace Vormkracht10\FilamentMails\Resources;
 
-use Filament\Infolists\Infolist;
-use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Vormkracht10\FilamentMails\Resources\EventResource\Pages\ListEvents;
-use Vormkracht10\Mails\Enums\WebhookEventType;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Infolists\Components\Grid;
 use Vormkracht10\Mails\Models\MailEvent;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Vormkracht10\Mails\Enums\WebhookEventType;
+use Filament\Infolists\Components\KeyValueEntry;
+use Vormkracht10\FilamentMails\Resources\EventResource\Pages\ListEvents;
 
 class EventResource extends Resource
 {
@@ -50,7 +54,92 @@ class EventResource extends Resource
     public static function infolist(Infolist $infolist): Infolist
     {
         return $infolist
-            ->schema([]);
+            ->schema([
+                Section::make(__('Event Details'))
+                    ->icon('heroicon-o-information-circle')
+                    ->collapsible()
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('type')
+                                    ->label(__('Type'))
+                                    ->badge()
+                                    ->color(fn(WebhookEventType $state): string => match ($state) {
+                                        WebhookEventType::DELIVERY => 'success',
+                                        WebhookEventType::CLICK => 'info',
+                                        WebhookEventType::OPEN => 'success',
+                                        WebhookEventType::BOUNCE => 'danger',
+                                        WebhookEventType::COMPLAINT => 'danger',
+                                        default => 'gray',
+                                    }),
+                                TextEntry::make('mail.subject')
+                                    ->label(__('Mail Subject')),
+                                TextEntry::make('occurred_at')
+                                    ->label(__('Occurred At'))
+                                    ->dateTime(),
+                            ]),
+
+                    ]),
+                Section::make(__('User Information'))
+                    ->icon('heroicon-o-user-circle')
+                    ->collapsible()
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('ip_address')
+                                    ->default(__('Unknown'))
+                                    ->label(__('IP Address')),
+                                TextEntry::make('hostname')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Hostname')),
+                                TextEntry::make('platform')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Platform')),
+                                TextEntry::make('os')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Operating System')),
+                                TextEntry::make('browser')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Browser')),
+                                TextEntry::make('user_agent')
+                                    ->default(__('Unknown'))
+                                    ->label(__('User Agent'))
+                                    ->limit(50)
+                                    ->tooltip(fn($state) => $state),
+                            ]),
+                    ]),
+                Section::make(__('Location'))
+                    ->icon('heroicon-o-map-pin')
+                    ->collapsible()
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('city')
+                                    ->default(__('Unknown'))
+                                    ->label(__('City')),
+                                TextEntry::make('country_code')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Country Code')),
+                            ]),
+                    ]),
+                Section::make(__('Additional Information'))
+                    ->icon('heroicon-o-information-circle')
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('link')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Link'))
+                                    ->url(fn($state) => $state)
+                                    ->openUrlInNewTab(),
+                                TextEntry::make('tag')
+                                    ->default(__('Unknown'))
+                                    ->label(__('Tag')),
+                                // KeyValueEntry::make('payload')
+                                //     ->label(__('Payload')),
+                            ]),
+                    ]),
+            ]);
     }
 
     public static function table(Table $table): Table
