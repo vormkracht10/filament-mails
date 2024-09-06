c24fccf8c61be5a04be5b12c6993878a3c543e52
Baspa <hello@baspa.dev>
2024-09-06 09:32:11 +0200

    Change order of stats

--- src/Widgets/MailStatsWidget.php
+++ src/Widgets/MailStatsWidget.php
@@ -12,18 +12,17 @@ class MailStatsWidget extends BaseWidget
 
     protected function getStats(): array
     {
-        $bouncedMails = Mail::where(fn ($query) => $query->softBounced()->orWhere(fn ($query) => $query->hardBounced()))->count();
+        $bouncedMails = Mail::where(fn($query) => $query->softBounced()->orWhere(fn($query) => $query->hardBounced()))->count();
         $openedMails = Mail::opened()->count();
         $deliveredMails = Mail::delivered()->count();
         $clickedMails = Mail::clicked()->count();
 
         $mailCount = Mail::count();
 
-        $widgets[] = Stat::make(__('Bounced'), number_format(($bouncedMails / $mailCount) * 100, 1) . '%')
-            ->label(__('Bounced'))
-            ->description($bouncedMails . ' ' . __('of') . ' ' . $mailCount . ' ' . __('emails'))
-            ->color('danger')
-            ->url(route('filament.admin.resources.mails.index', ['activeTab' => 'bounced']));
+        $widgets[] = Stat::make(__('Delivered'), number_format(($deliveredMails / $mailCount) * 100, 1) . '%')
+            ->label(__('Delivered'))
+            ->description($deliveredMails . ' ' . __('of') . ' ' . $mailCount . ' ' . __('emails'))
+            ->color('success');
 
         $widgets[] = Stat::make(__('Opened'), number_format(($openedMails / $mailCount) * 100, 1) . '%')
             ->label(__('Opened'))
@@ -36,10 +35,11 @@ class MailStatsWidget extends BaseWidget
             ->description($clickedMails . ' ' . __('of') . ' ' . $mailCount . ' ' . __('emails'))
             ->color('clicked');
 
-        $widgets[] = Stat::make(__('Delivered'), number_format(($deliveredMails / $mailCount) * 100, 1) . '%')
-            ->label(__('Delivered'))
-            ->description($deliveredMails . ' ' . __('of') . ' ' . $mailCount . ' ' . __('emails'))
-            ->color('success');
+        $widgets[] = Stat::make(__('Bounced'), number_format(($bouncedMails / $mailCount) * 100, 1) . '%')
+            ->label(__('Bounced'))
+            ->description($bouncedMails . ' ' . __('of') . ' ' . $mailCount . ' ' . __('emails'))
+            ->color('danger')
+            ->url(route('filament.admin.resources.mails.index', ['activeTab' => 'bounced']));
 
         return $widgets;
     }
